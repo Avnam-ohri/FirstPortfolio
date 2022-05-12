e80cccf108b325c4ce10b09c8f2549cc9d8288fc
Avnam-ohri <avnamohri@gmail.com>
2022-05-12 10:42:48 +0530

    form update

--- form.php
+++ form.php
@@ -1,14 +1,14 @@
 <?php
 if (isset($_POST['submit'])){
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
-    $message = $_POST['Message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
     $mailTo = "avnamohri@outlook.com";
     $headers ="From: ".$email;
     $txt = "You have reccived a mail from ".$name. "\n\n".$message;
 
     mail($mailTo,$txt,$headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }
 ?>
