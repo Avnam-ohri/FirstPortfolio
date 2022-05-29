9a7dfac4941075c49779592faf6ce02f31565290
Avnam-ohri <avnamohri@gmail.com>
2022-05-29 15:12:52 +0530

    updated resume

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+// const request = require("request")
+const https = require("https");
+app.use(express.static("public"));
+
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.get("/",function(req,res){
@@ -8,10 +13,48 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    console.log(req.body)
-res.send("<h1>Thanks for posting</h1>")
+ const name = req.body.name
+ const email = req.body.email
+ const message = req.body.message
+
+ const data = {
+     members : [
+        {
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+            FNAME:name,
+            LNAME:message
+                
+            }
+        } 
+     ]
+ };
+
+ const jsonData = JSON.stringify(data)
+const url = "https://us8.api.mailchip.com/3.0/lists/83f1c1205d"
+ 
+const options = {
+    method:"GET",
+    auth:"Avnam:c0414570f10bef92d1b94484ab54e032-us8"
+}
+
+const request = https.request(url,options,function(response){
+    response.on("data",function(data){
+        console.log(JSON.parse(data));
+
+        
+    })
+ });
+
+request.write(jsonData);
+request.end()
 })
 
-// app.listen(3000,function(){
-//     console.log("server begins here")
-// })
+app.listen(3000,function(){
+    console.log("server begins here")
+})
+
+
+//c0414570f10bef92d1b94484ab54e032-us8 - API key
+//83f1c1205d - unique id
